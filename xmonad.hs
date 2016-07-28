4251c736c01c51725751746b23ffc4d14a29cb4f
Bernd Schwarzenbacher <bernd.schwb@yahoo.com>
2016-07-28 21:22:11 +0200

    XMonad: auto float netgen windows

--- xmonad.hs
+++ xmonad.hs
@@ -1,14 +1,15 @@
 import XMonad
+import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Config.Desktop
 
 baseConfig = desktopConfig
 
 main = xmonad $ baseConfig
-    { modMask = mod4Mask
+    { manageHook = ((className =? "Toplevel") --> doFloat) <+> manageHook baseConfig
+    , modMask = mod4Mask
     , terminal = "urxvt"
     }
     `additionalKeys`
     [ ((mod4Mask, xK_p), spawn "dmenu_recent_aliases")
     ]
-
