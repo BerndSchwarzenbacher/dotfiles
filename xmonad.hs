55f6dda92ea71fa54dd2ec63f4faa00e3d531014
Bernd Schwarzenbacher <bernd.schwb@yahoo.com>
2019-06-25 14:47:24 +0200

    Adapt xmonad to company setup

--- xmonad.hs
+++ xmonad.hs
@@ -1,17 +1,20 @@
 import XMonad
-import XMonad.Hooks.ManageDocks
+import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Config.Gnome
 
 baseConfig = gnomeConfig
 
-main = xmonad $ baseConfig
-    { manageHook = composeAll
-      [ (className =? "Toplevel") --> doFloat
-      , (className =? "Vite")     --> doFloat
-      ] <+> manageHook baseConfig
-    , modMask = mod4Mask
-    }
-    `additionalKeys`
-    [ ((mod4Mask, xK_p), spawn "dmenu_recent_aliases")
+main = do
+    xmonad $ gnomeConfig {
+      modMask = mod4Mask
+      } `additionalKeys` myKeys
+
+myKeys =
+    [
+      ((mod4Mask, xK_p), spawn "dmenu_recent_aliases")
     ]
+    ++
+    [((m .|. mod4Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e, xK_r] [1,2,0] -- was [0..] *** change to match your screen order ***
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
